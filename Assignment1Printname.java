33dc8881dfcfce99b6478a774657c7e8308c4c87
Priya <priyalangalia86@gmail.com>
2024-03-01 12:39:37 +0000

    Assignment 1

---
+++ Assignment1Printname.java
@@ -0,0 +1,13 @@
+package basicsofjava;
+// Assignment 1 - Print your name
+public class Assignment1Printname {
+	
+		
+public static void main(String[] args)
+{
+	
+	System.out.println("priya");
+	
+	
+}
+}
